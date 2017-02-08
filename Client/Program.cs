f6559950a949c46995a7854b1f19412815fa9c68
Robert Ault <robault@live.com>
2017-02-07 19:29:23 -0800

    Initial check in

---
+++ Client/Program.cs
@@ -0,0 +1,82 @@
+﻿using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static string client = "192.168.1.139";
+        static int port = 10100;
+
+        static DirectSoundOut waveOut;
+        static BufferedWaveProvider waveProvider;
+        static UdpClient udpListener;
+        static INetworkChatCodec selectedCodec;
+        static bool connected;
+
+        static void Main(string[] args)
+        {
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse(client), port);
+            selectedCodec = new G722ChatCodec();
+            Connect(endPoint, 0, selectedCodec);
+            Console.WriteLine(String.Format("Connected to: {0}:{1}", client, port));
+            Console.ReadLine();
+            Disconnect();
+        }
+
+        static void Connect(IPEndPoint endPoint, int inputDeviceNumber, INetworkChatCodec codec)
+        {
+            udpListener = new UdpClient();
+            // if running both from the same computer: (otherwise comment these next two out)
+            udpListener.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+            udpListener.Client.Bind(endPoint);
+
+            waveOut = new DirectSoundOut();
+            waveProvider = new BufferedWaveProvider(codec.RecordFormat);
+            waveOut.Init(waveProvider);
+            waveOut.Play();
+
+            var state = new ListenerThreadState { Codec = codec, EndPoint = endPoint };
+            ThreadPool.QueueUserWorkItem(ListenerThread, state);
+        }
+
+        static void Disconnect()
+        {
+            udpListener.Close();
+            waveOut.Dispose();
+            selectedCodec.Dispose();
+        }
+
+        class ListenerThreadState
+        {
+            public IPEndPoint EndPoint { get; set; }
+            public INetworkChatCodec Codec { get; set; }
+        }
+
+        static void ListenerThread(object state)
+        {
+            var listenerThreadState = (ListenerThreadState)state;
+            var endPoint = listenerThreadState.EndPoint;
+            try
+            {
+                while (connected)
+                {
+                    byte[] b = udpListener.Receive(ref endPoint);
+                    byte[] decoded = listenerThreadState.Codec.Decode(b, 0, b.Length);
+                    waveProvider.AddSamples(decoded, 0, decoded.Length);
+                }
+            }
+            catch (SocketException)
+            {
+                // usually not a problem - just means we have disconnected
+            }
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,60 @@
+﻿using Client;
+using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Program
+    {
+        static string client = "192.168.1.139";
+        static int port = 10100;
+
+        static WaveIn waveIn;
+        static UdpClient udpSender;
+        static INetworkChatCodec selectedCodec;
+
+        static void Main(string[] args)
+        {
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse(client), port);
+            selectedCodec = new G722ChatCodec();
+            Connect(endPoint, 0, selectedCodec);
+            Console.WriteLine(String.Format("Connected to: {0}:{1}", client, port));
+            Console.ReadLine();
+            Disconnect();
+        }
+
+        static void Connect(IPEndPoint endPoint, int inputDeviceNumber, INetworkChatCodec codec)
+        {
+            waveIn = new WaveIn();
+            waveIn.BufferMilliseconds = 50;
+            //Microphone.DeviceNumber = inputDeviceNumber; // or just set default device in windows
+            waveIn.WaveFormat = codec.RecordFormat;
+            waveIn.DataAvailable += waveIn_DataAvailable;
+            udpSender = new UdpClient();
+            udpSender.Connect(endPoint);
+            waveIn.StartRecording();
+        }
+
+        static void Disconnect()
+        {
+            waveIn.DataAvailable -= waveIn_DataAvailable;
+            waveIn.StopRecording();
+            udpSender.Close();
+            waveIn.Dispose();
+            selectedCodec.Dispose();
+        }
+        
+        static void waveIn_DataAvailable(object sender, WaveInEventArgs e)
+        {
+            byte[] encoded = selectedCodec.Encode(e.Buffer, 0, e.BytesRecorded);
+            udpSender.Send(encoded, encoded.Length);
+        }
+    }
+}
